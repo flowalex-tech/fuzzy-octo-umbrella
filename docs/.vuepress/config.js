42f8184452073f9114a46cf6f6f67df80604b1f4
Alex <flowalex@vivaldi.net>
2021-04-19 17:55:20 -0500

    Starting Site from scratch

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+// .vuepress/config.js
+module.exports = {
+    title: 'Documentation',
+    theme: 'vuepress-theme-material-free',
+    themeConfig: {
+      logo: '/assets/img/my-logo.png',
+      authors: {
+        'Alex Wolf': {
+          image: '/assets/img/avatar.png',
+        },
+      },
+      nav: [
+        { label: 'Containers', path: '/containers', icon: 'docker' },
+        { label: 'Python', path: '/python', icon: 'python' },
+        { label: 'About the author', path: '/about.html', icon: 'person' },
+        { label: 'Tags', path: '/tag', icon: 'category' },
+      ],
+      footer: {
+        text: 'Copyright 2021 All Rights Reserverd',
+      },
+      tags: {
+        'vuejs': 'Vue.js',
+        'ruby-on-rails': 'Ruby on Rails',
+        'nodejs': 'Node.js',
+      },
+      locales: {
+        default: 'en', // or 'pt-BR'
+      },
+    },
+  };
